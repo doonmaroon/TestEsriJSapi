8b0c14b3b5628d63cd4fea40a166d9404a42e4d1
doonmaroon <rarthur138@gmail.com>
2021-06-16 23:58:55 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+// Import stylesheets
+import './style.css';
+
+// Write Javascript code!
+const appDiv = document.getElementById('app');
+appDiv.innerHTML = `<h1>Find Your Location</h1>`;
+
+const appDiv2 = document.getElementById('map');
+appDiv2.innerHTML = `<h2>test app</h2>`;
+
